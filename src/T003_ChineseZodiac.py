46ab84fc76caaea67a7087cf1d55d19d9a766640
xichengxml <xichengxml@163.com>
2019-09-22 16:37:27 +0800

    i

--- src/T003_ChineseZodiac.py
+++ src/T003_ChineseZodiac.py
@@ -3,11 +3,11 @@
 # 列表：[0, "abcd"]
 # 元组: ("a", "b")
 
-chineseZodiac = "鼠牛虎兔龙蛇马羊猴鸡狗猪"
+chineseZodiac = "猴鸡狗猪鼠牛虎兔龙蛇马羊"
 
 print(chineseZodiac[0])
 print(chineseZodiac[0:4])
 print(chineseZodiac[-1])
 
-year = 2019
+year = 1994
 print(chineseZodiac[year % 12])
